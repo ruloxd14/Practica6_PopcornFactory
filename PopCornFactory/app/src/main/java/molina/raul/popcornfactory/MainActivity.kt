549e20028fc88c8d9c475823831d434e427425cf
Raul <ruloxd28@gmail.com>
2021-03-08 04:21:17 -0700

    2
    .

--- PopCornFactory/app/src/main/java/molina/raul/popcornfactory/MainActivity.kt
+++ PopCornFactory/app/src/main/java/molina/raul/popcornfactory/MainActivity.kt
@@ -1,13 +1,19 @@
 package molina.raul.popcornfactory
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import kotlinx.android.synthetic.main.pelicula.view.*
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-    
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        botonP.setOnClickListener() {
+            var intent: Intent = Intent(this, Catalogo::class.java)
+            startActivity(intent)
+        }
     }
 }
